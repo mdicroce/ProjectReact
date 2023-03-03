c864a46822bf75525f47e9fab42f73357d06fcd4
Matias Di Croce <matias.dicroce@amcnetworks.com>
2023-03-03 17:08:18 -0300

    config eslint y react-router

---
+++ src/routes/detail.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface Props {
+  anime: string
+}
+
+export const Detail: React.FC<Props> = ({ anime }) => {
+  return <div>detail</div>
+}
---
+++ src/routes/home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface Props {
+  anime: string
+}
+
+export const Home: React.FC<Props> = ({ anime }) => {
+  return <div>Home</div>
+}
